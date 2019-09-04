04be96392771c33a33a5b971281f0b02dc3b266b
Sarvani Yakkali <sarvani.y@comakeit.com>
2019-09-04 11:49:10 +0530

    Add project files.

---
+++ Enums/RoleTypeEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CoProcessApplication.Enums
+{
+    public enum RoleTypeEnum
+    {
+        productowner,
+        Architect,
+        Developer,
+        QA
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CoProcessApplication.MvcApplication" Language="C#" %>
